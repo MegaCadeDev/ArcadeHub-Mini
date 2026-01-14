c6f4cc3079483d04c61bf15f4d770a5bf96288bb
Gary Likovic <81448614+MegaCadeDev@users.noreply.github.com>
2026-01-14 09:39:12 -0500

    Add Bluetooth Switch controller support

---
+++ src/Controllers/Bluetooth/Switch.c
@@ -0,0 +1,373 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <pico/cyw43_arch.h>
+#include <pico/multicore.h>
+#include <pico/async_context.h>
+#include <uni.h>
+
+#include "sdkconfig.h"
+#include "uni_hid_device.h"
+#include "uni_log.h"
+#include "usb.h"
+#include "report.h"
+#include "Switch.h"
+#include "WiiU.h"
+#include "Wii.h"
+
+// Sanity check
+#ifndef CONFIG_BLUEPAD32_PLATFORM_CUSTOM
+#error "Pico W must use BLUEPAD32_PLATFORM_CUSTOM"
+#endif
+
+#define AXIS_DEADZONE 0xa
+
+// Declarations
+static void trigger_event_on_gamepad(uni_hid_device_t *d);
+SwitchOutReport report[CONFIG_BLUEPAD32_MAX_DEVICES];
+SwitchIdxOutReport idx_r;
+uint8_t connected_controllers;
+
+// Disconnect Controller Combo declarations
+#define DISCONNECT_COMBO (MISC_BUTTON_HOME | MISC_BUTTON_BACK)
+static bool disconnect_combo_active[CONFIG_BLUEPAD32_MAX_DEVICES] = { false };
+
+// Controlller Connected Check Switch
+typedef enum {
+    CTL_UNKNOWN = 0,
+    CTL_WIIU_PRO,
+	CTL_WII,
+    CTL_OTHER,
+} controller_type_t;
+
+
+// Helper functions
+static void empty_gamepad_report(SwitchOutReport *gamepad)
+{
+	gamepad->buttons = 0;
+	gamepad->hat = SWITCH_HAT_NOTHING;
+	gamepad->lx = SWITCH_JOYSTICK_MID;
+	gamepad->ly = SWITCH_JOYSTICK_MID;
+	gamepad->rx = SWITCH_JOYSTICK_MID;
+	gamepad->ry = SWITCH_JOYSTICK_MID;
+}
+
+uint8_t convert_to_switch_axis(int32_t bluepadAxis)
+{
+	// bluepad32 reports from -512 to 511 as int32_t
+	// switch reports from 0 to 255 as uint8_t
+
+	bluepadAxis += 513;  // now max possible is 1024
+	bluepadAxis /= 4;    // now max possible is 255
+
+	if (bluepadAxis < SWITCH_JOYSTICK_MIN)
+		bluepadAxis = 0;
+	else if ((bluepadAxis > (SWITCH_JOYSTICK_MID - AXIS_DEADZONE)) &&
+	         (bluepadAxis < (SWITCH_JOYSTICK_MID + AXIS_DEADZONE))) {
+		bluepadAxis = SWITCH_JOYSTICK_MID;
+	} else if (bluepadAxis > SWITCH_JOYSTICK_MAX)
+		bluepadAxis = SWITCH_JOYSTICK_MAX;
+
+	return (uint8_t) bluepadAxis;
+}
+
+static void fill_gamepad_report(int idx, uni_gamepad_t *gp, controller_type_t type)
+{
+	empty_gamepad_report(&report[idx]);
+
+	// face buttons
+	if ((gp->buttons & BUTTON_A)) {
+		report[idx].buttons |= SWITCH_MASK_A;
+	}
+	if ((gp->buttons & BUTTON_B)) {
+		report[idx].buttons |= SWITCH_MASK_B;
+	}
+	if ((gp->buttons & BUTTON_X)) {
+		report[idx].buttons |= SWITCH_MASK_X;
+	}
+	if ((gp->buttons & BUTTON_Y)) {
+		report[idx].buttons |= SWITCH_MASK_Y;
+	}
+
+	// shoulder buttons
+	if (type == CTL_WII){
+		if ((gp->buttons & WII_MASK_L)) {
+			report[idx].buttons |= SWITCH_MASK_L;
+		}
+		if ((gp->buttons & WII_MASK_R)) {
+			report[idx].buttons |= SWITCH_MASK_R;
+		}
+	}
+	else {
+		if ((gp->buttons & BUTTON_SHOULDER_L)) {
+			report[idx].buttons |= SWITCH_MASK_L;
+		}
+		if ((gp->buttons & BUTTON_SHOULDER_R)) {
+			report[idx].buttons |= SWITCH_MASK_R;
+		}
+	}
+
+
+	// dpad
+	switch (gp->dpad) {
+	case DPAD_UP:
+		report[idx].hat = SWITCH_HAT_UP;
+		break;
+	case DPAD_DOWN:
+		report[idx].hat = SWITCH_HAT_DOWN;
+		break;
+	case DPAD_LEFT:
+		report[idx].hat = SWITCH_HAT_LEFT;
+		break;
+	case DPAD_RIGHT:
+		report[idx].hat = SWITCH_HAT_RIGHT;
+		break;
+	case DPAD_UP | DPAD_RIGHT:
+		report[idx].hat = SWITCH_HAT_UPRIGHT;
+		break;
+	case DPAD_DOWN | DPAD_RIGHT:
+		report[idx].hat = SWITCH_HAT_DOWNRIGHT;
+		break;
+	case DPAD_DOWN | DPAD_LEFT:
+		report[idx].hat = SWITCH_HAT_DOWNLEFT;
+		break;
+	case DPAD_UP | DPAD_LEFT:
+		report[idx].hat = SWITCH_HAT_UPLEFT;
+		break;
+	default:
+		report[idx].hat = SWITCH_HAT_NOTHING;
+		break;
+	}
+
+	// sticks
+	report[idx].lx = convert_to_switch_axis(gp->axis_x);
+	report[idx].ly = convert_to_switch_axis(gp->axis_y);
+	report[idx].rx = convert_to_switch_axis(gp->axis_rx);
+	report[idx].ry = convert_to_switch_axis(gp->axis_ry);
+	if ((gp->buttons & BUTTON_THUMB_L))
+		report[idx].buttons |= SWITCH_MASK_L3;
+	if ((gp->buttons & BUTTON_THUMB_R))
+		report[idx].buttons |= SWITCH_MASK_R3;
+
+	// triggers
+	// Check if specific controller	
+	if (type == CTL_WIIU_PRO || type == CTL_WII) {
+		if (gp->buttons & WIIU_MASK_ZR)
+			report[idx].buttons |= SWITCH_MASK_ZR;
+		if (gp->buttons & WIIU_MASK_ZL)
+			report[idx].buttons |= SWITCH_MASK_ZL;
+	} else {
+		if (gp->brake)
+			report[idx].buttons |= SWITCH_MASK_ZL;
+		if (gp->throttle)
+			report[idx].buttons |= SWITCH_MASK_ZR;
+	}
+
+
+
+	// misc buttons
+	if (gp->misc_buttons & MISC_BUTTON_SYSTEM)
+		report[idx].buttons |= SWITCH_MASK_HOME;
+	if (gp->misc_buttons & MISC_BUTTON_CAPTURE)
+		report[idx].buttons |= SWITCH_MASK_CAPTURE;
+	if (gp->misc_buttons & MISC_BUTTON_BACK)
+		report[idx].buttons |= SWITCH_MASK_MINUS;
+	if (gp->misc_buttons & MISC_BUTTON_HOME)
+		report[idx].buttons |= SWITCH_MASK_PLUS;
+}
+
+static void set_led_status() {
+	if (connected_controllers == 0)
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+	else
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+}
+
+//
+// Platform Overrides
+//
+static void pico_switch_platform_init(int argc, const char** argv) {
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+
+    logi("my_platform: init()\n");
+
+	connected_controllers = 0;
+
+	uni_gamepad_mappings_t mappings = GAMEPAD_DEFAULT_MAPPINGS;
+
+	// remaps
+	mappings.button_b = UNI_GAMEPAD_MAPPINGS_BUTTON_A;
+	mappings.button_a = UNI_GAMEPAD_MAPPINGS_BUTTON_B;
+	mappings.button_y = UNI_GAMEPAD_MAPPINGS_BUTTON_X;
+	mappings.button_x = UNI_GAMEPAD_MAPPINGS_BUTTON_Y;
+
+	uni_gamepad_set_mappings(&mappings);
+
+	idx_r.idx = 0;
+	idx_r.report.buttons = 0;
+	idx_r.report.hat = SWITCH_HAT_NOTHING;
+	idx_r.report.lx = 0;
+	idx_r.report.ly = 0;
+	idx_r.report.rx = 0;
+	idx_r.report.ry = 0;
+	set_global_gamepad_report(&idx_r);
+
+}
+
+static void pico_switch_platform_on_init_complete(void) {
+    logi("my_platform: on_init_complete()\n");
+
+    // Safe to call "unsafe" functions since they are called from BT thread
+
+    // Start scanning
+    uni_bt_enable_new_connections_safe(true);
+
+    // Based on runtime condition you can delete or list the stored BT keys.
+    if (1)
+        uni_bt_del_keys_unsafe();
+    else
+        uni_bt_list_keys_unsafe();
+
+    // Turn off LED once init is done.
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+
+	logi("BLUEPAD: ready to fill reports");
+	multicore_fifo_push_blocking(0); // signal other core to start reading
+}
+
+static void pico_switch_platform_on_device_connected(uni_hid_device_t* d) {
+    logi("my_platform: device connected: %p\n", d);
+}
+
+static void pico_switch_platform_on_device_disconnected(uni_hid_device_t* d) {
+    logi("my_platform: device disconnected: %p\n", d);
+	// NOT WORKING?
+	// This is complicated. uni_hid_device_get_idx_for_instance 
+	// no longer gives us the index once the device is disconnected.
+	// We assume in this case that a device disconnecting is in a state of no gameplay
+	// so we set momentarelly all gamepad reports to 0.
+	// If this disconnection happens during gameplay, the gamepad would be stuck in the last state.
+	for (int i = 0; i < CONFIG_BLUEPAD32_MAX_DEVICES; i++) {
+		empty_gamepad_report(&report[i]);
+		idx_r.idx = i;
+		idx_r.report = report[i];
+		set_global_gamepad_report(&idx_r);
+	}
+	connected_controllers--;
+	set_led_status();
+}
+
+static uni_error_t pico_switch_platform_on_device_ready(uni_hid_device_t* d) {
+    logi("my_platform: device ready: %p\n", d);
+
+    controller_type_t type = CTL_OTHER;
+
+    if (d->vendor_id == WII_U_PRO_VID &&
+        d->product_id == WII_U_PRO_PID) {
+        type = CTL_WIIU_PRO;
+    }
+	else if (d->vendor_id == WII_VID && d->product_id == WII_PID){
+		type = CTL_WII;
+	}
+
+    // Store controller type in platform_data[0]
+    d->platform_data[0] = (uint8_t)type;
+
+	connected_controllers++;
+	set_led_status();
+    return UNI_ERROR_SUCCESS;
+}
+
+static void pico_switch_platform_on_controller_data(uni_hid_device_t* d, uni_controller_t* ctl)
+{
+	if (ctl->klass != UNI_CONTROLLER_CLASS_GAMEPAD) {
+		return;
+	}
+
+	uni_gamepad_t *gp;
+	uint8_t idx = uni_hid_device_get_idx_for_instance(d);
+
+	gp = &ctl->gamepad;
+
+	// START + SELECT Button combo detection
+	bool combo_pressed = (gp->misc_buttons & DISCONNECT_COMBO) == DISCONNECT_COMBO;
+
+	if (combo_pressed && !disconnect_combo_active[idx]) {
+		disconnect_combo_active[idx] = true;
+
+		logi("Disconnect combo pressed on the controller %d\n", idx);
+
+		// Disconnect the controller
+		uni_hid_device_disconnect(d);
+
+		// Start scanning for a new controller
+		uni_bt_enable_new_connections_safe(true);
+
+		return;
+	}
+
+	if (!combo_pressed) {
+		disconnect_combo_active[idx] = false;
+	}
+
+	controller_type_t type = (controller_type_t)d->platform_data[0];
+	fill_gamepad_report(idx, gp, type);
+	idx_r.idx = idx;
+	idx_r.report = report[idx];
+	set_global_gamepad_report(&idx_r);
+}
+
+static const uni_property_t* pico_switch_platform_get_property(uni_property_idx_t idx) {
+    // Deprecated
+    ARG_UNUSED(idx);
+    return NULL;
+}
+
+static void pico_switch_platform_on_oob_event(uni_platform_oob_event_t event, void* data) {
+	ARG_UNUSED(event);
+	ARG_UNUSED(data);
+	return;
+}
+
+//
+// Helpers - UNUSED
+//
+static void trigger_event_on_gamepad(uni_hid_device_t* d) {
+    if (d->report_parser.set_player_leds != NULL) {
+        static uint8_t led = 0;
+        led += 1;
+        led &= 0xf;
+        d->report_parser.set_player_leds(d, led);
+    }
+
+    if (d->report_parser.set_lightbar_color != NULL) {
+        static uint8_t red = 0x10;
+        static uint8_t green = 0x20;
+        static uint8_t blue = 0x40;
+
+        red += 0x10;
+        green -= 0x20;
+        blue += 0x40;
+        d->report_parser.set_lightbar_color(d, red, green, blue);
+    }
+}
+
+//
+// Entry Point
+//
+struct uni_platform* get_my_platform(void) {
+    static struct uni_platform plat = {
+        .name = "My Platform",
+        .init = pico_switch_platform_init,
+        .on_init_complete = pico_switch_platform_on_init_complete,
+        .on_device_connected = pico_switch_platform_on_device_connected,
+        .on_device_disconnected = pico_switch_platform_on_device_disconnected,
+        .on_device_ready = pico_switch_platform_on_device_ready,
+        .on_oob_event = pico_switch_platform_on_oob_event,
+        .on_controller_data = pico_switch_platform_on_controller_data,
+        .get_property = pico_switch_platform_get_property,
+    };
+
+    return &plat;
+}
